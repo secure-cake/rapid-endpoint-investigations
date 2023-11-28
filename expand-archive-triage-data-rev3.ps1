c8bc842b613228d4207dbcab18c0ffcc7e292af8
Secure Cake <49917607+secure-cake@users.noreply.github.com>
2023-11-28 12:04:26 -0500

    Add files via upload

---
+++ expand-archive-triage-data-rev3.ps1
@@ -0,0 +1,9 @@
+﻿#Must use PowerShell 7.x to avoid long path/filename output issues
+$casename = '2023-0829-abc'
+$compressed_triage_data = "D:\cases\$casename\triage_data"
+$unzipped_triage_data = "D:\cases\$casename\triage_data"
+
+(get-childitem -path $compressed_triage_data -filter *.zip).basename | ForEach-Object {
+    
+    Expand-Archive -path $compressed_triage_data\$_.zip -DestinationPath $unzipped_triage_data\$_ -Force
+}

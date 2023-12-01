ec4e808ccd9471ce1bc7a3fb9ed1b470e443a390
Secure Cake <49917607+secure-cake@users.noreply.github.com>
2023-12-01 09:42:55 -0500

    Update expand-archive-triage-data-rev3.ps1

--- expand-archive-triage-data-rev3.ps1
+++ expand-archive-triage-data-rev3.ps1
@@ -1,4 +1,5 @@
 ﻿#Must use PowerShell 7.x to avoid long path/filename output issues
+#Change the three variables below to match your file/folder paths
 $casename = '2023-0829-abc'
 $compressed_triage_data = "D:\cases\$casename\triage_data"
 $unzipped_triage_data = "D:\cases\$casename\triage_data"

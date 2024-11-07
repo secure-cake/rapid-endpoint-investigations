acc02a91623efe2912706c4691a0386db338c248
Secure Cake <49917607+secure-cake@users.noreply.github.com>
2024-11-07 12:25:53 -0700

    Update KAPE_Rapid_Triage_Excel_Output_Rev3.ps1
    Added a couple VR modules (services, dns cache), as well as a few filelisting file extensions.

--- KAPE_Rapid_Triage_Excel_Output_Rev3.ps1
+++ KAPE_Rapid_Triage_Excel_Output_Rev3.ps1
@@ -13,7 +13,7 @@ $triage_data_directory = "d:\cases\$casename\triage_data"
 $kape_destination_directory = "d:\cases\$casename\kape_output"
 
 #change the startdate below for evtx triage, change includedevents as desired - must create evtxecmd-triage kape module with variables to use
-$startdate = '2023-12-15'
+$startdate = '2024-11-1'
 $includedevents = '1102,1116,1117,4624,4625,4720,4722,4724,4738,5001,5007,7045,4104,4698'
 $csvf = "evtx-triage-output.csv"
 
@@ -29,11 +29,13 @@ $csvf = "evtx-triage-output.csv"
     Invoke-Kape -msource $triage_data_directory\$_\uploads\auto\C%3A -mdest $kape_destination_directory\$_ -Module !EvtxECmd-Triage -mvars startdate:$startdate^includedevents:$includedevents^filename:$csvf
     #Perofrms $MFT file listing analysis, output to mft-filelisting sub-directory ...not rolled up into Excel
     Invoke-Kape -msource $triage_data_directory\$_\uploads\ntfs\%5C%5C.%5CC%3A -mdest "$kape_destination_directory\$_-mft-filelisting" -Module 'MFTECmd_$MFT_FileListing' -mvars csv
-    get-content "$kape_destination_directory\$_-mft-filelisting\FileSystem\*FileListing.csv" | ConvertFrom-Csv | Where-Object {($_.Extension -eq ".exe") -or ($_.Extension -eq ".zip") -or ($_.Extension -eq ".ps1") -or ($_.Extension -eq ".js") -or ($_.Extension -eq ".dll")} | Export-Csv $kape_destination_directory\$_'-mft_filelisting_executable_files.csv' -NoTypeInformation
+    get-content "$kape_destination_directory\$_-mft-filelisting\FileSystem\*FileListing.csv" | ConvertFrom-Csv | Where-Object {($_.Extension -eq ".exe") -or ($_.Extension -eq ".zip") -or ($_.Extension -eq ".ps1") -or ($_.Extension -eq ".js") -or ($_.Extension -eq ".dll") -or ($_.Extension -eq ".vbs") -or ($_.Extension -eq ".cmd") -or ($_.Extension -eq ".bat") -or ($_.Extension -eq ".xml") -or ($_.Extension -eq ".7z") -or ($_.Extension -eq ".rar")} | Export-Csv $kape_destination_directory\$_'-mft_filelisting_executable_files.csv' -NoTypeInformation
     #Copies triage-collection "results" to kape_output directory for review
     Copy-Item $triage_data_directory\$_\results\*Netstat.csv  $kape_destination_directory\$_'-netstat-encriched.csv'
     Copy-Item $triage_data_directory\$_\results\*Autoruns.csv  $kape_destination_directory\$_'-autoruns.csv'
     Copy-Item $triage_data_directory\$_\results\*pslist.csv  $kape_destination_directory\$_'-pslist.csv'
+    Copy-Item $triage_data_directory\$_\results\*Services.csv  $kape_destination_directory\$_'-services.csv'
+    Copy-Item $triage_data_directory\$_\results\*DNSCache.csv  $kape_destination_directory\$_'-dnscache.csv'
     #Combines all csv and xls files into a workbook per station...not including MFT
     $ExcelObject=New-Object -ComObject excel.application
     $ExcelObject.visible=$true

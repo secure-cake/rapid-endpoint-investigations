ab2452d1ac6c799308ebd4e791e483e290929f4b
Secure Cake <49917607+secure-cake@users.noreply.github.com>
2026-03-31 16:30:09 -0600

    Create vr_catscale_extract.sh

---
+++ linux-mac/vr_catscale_extract.sh
@@ -0,0 +1,6 @@
+#stage your VR collections (zip files) in a directory (triage_data_linux)
+#execute this to extract each zip file to a folder based on filename (host)
+find ./ -name "*.zip" | while read filename; do 
+  basename="${filename%.*}" 
+  7z x "$filename" -o"./$basename" ; find "./$basename" -name "*.tar.gz" -exec 7z x {} -o"./$basename" \; ; find "./$basename" -name "catscale_collection.tar" -exec tar -xf {} --strip-components=2 -C "./$basename" \;
+done
